bb74f114e1a718fc164af2a6653513eb41a1caea
tim lindner <tlindner@macmess.org>
2020-10-31 08:24:31 -0700

    Initial Commit

---
+++ TransLit/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  TransLit
+//
+//  Created by tim lindner on 10/31/20.
+//  Copyright © 2020 tim lindner. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
